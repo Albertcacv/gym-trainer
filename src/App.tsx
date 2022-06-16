f61086b2496fd9704c52fee2a374e086fff6a085
albert <55817155+Albertcacv@users.noreply.github.com>
2022-06-16 15:18:05 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Box } from '@mui/material'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  )
+}
+
+export default App
